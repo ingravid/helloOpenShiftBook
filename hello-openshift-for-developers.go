03e70d63db0efb1afd2936668ed6278ee88617bc
ingravid <89873886+ingravid@users.noreply.github.com>
2022-09-19 23:03:35 +0200

    Changed the outcome message for lab

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World OpenShift for Developers!"
+		response = "Hello World OpenShift for OpenShift Developers in a Opneasdkf worrrld!"
 	}
 
 	fmt.Fprintln(w, response)
